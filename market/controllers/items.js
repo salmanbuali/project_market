7cd8f61bf04584100746d76fde74bd5ed98c3cf7
najeeb2442 <s.najib2442@gmail.com>
2024-02-11 10:50:40 +0300

    added warning for the user if he isn't logged in

--- market/controllers/items.js
+++ market/controllers/items.js
@@ -3,15 +3,20 @@ const User = require("../models/user")
 
 const newItem = async (req, res) => {
   try {
-    req.body.sold = false
-    req.body.seller = req.user._id
-    theNewReturenedItem = await Item.create(req.body)
-    // needs to update user items
-    await User.updateOne(
-      { _id: req.user._id },
-      { $push: { items: theNewReturenedItem._id } }
-    )
-    res.redirect("/items")
+    if (req.user._id) {
+      req.body.sold = false
+      req.body.seller = req.user._id
+      theNewReturenedItem = await Item.create(req.body)
+      // needs to update user items
+      await User.updateOne(
+        { _id: req.user._id },
+        { $push: { items: theNewReturenedItem._id } }
+      )
+      res.redirect("/items")
+    } else {
+      res.redirect("/items/new")
+      alert("you are not logged in")
+    }
   } catch (err) {
     res.render("error", { err })
   }
@@ -19,7 +24,13 @@ const newItem = async (req, res) => {
 
 const createItemPage = async (req, res) => {
   try {
-    res.render("items/new")
+    if (req.user) {
+      res.render("items/new")
+    } else {
+      const message = "you are not logged in"
+      // alert("you are not logged in")
+      res.redirect("/items?message=" + message)
+    }
   } catch (err) {
     res.render("error", { err })
   }
@@ -28,8 +39,11 @@ const createItemPage = async (req, res) => {
 const index = async (req, res) => {
   try {
     let items = await Item.find()
-    res.render("items/",{items})
-
+    let message = ""
+    if (req.query.message) {
+      message = req.query.message
+    }
+    res.render("items/index", { items, message })
   } catch (error) {
     res.render("error", { err })
   }
