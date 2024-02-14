e2d6b0cb6be617d47a25e68ada643dca310934d8
najeeb2442 <s.najib2442@gmail.com>
2024-02-14 16:50:01 +0300

    delete comment

--- market/routes/comments.js
+++ market/routes/comments.js
@@ -5,7 +5,10 @@ const commentsController = require("../controllers/comments")
 // router.get("/show/:id", commentsController.show)
 // router.get('/show/65c873af04271ac777f53f3a', itemsController.show)
 
-// POST /planets/:planetId/plants
-router.post('/items/show/:id', commentsController.createComment);
+// POST /items/show/:id
+router.post("/items/show/:id", commentsController.createComment)
+
+// get /planets/:planetId/plants
+router.delete("/items/comments/:id", commentsController.deleteComment)
 
 module.exports = router
